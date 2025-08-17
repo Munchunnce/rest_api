0aeed9f3c25d2e1844e0589d526db136d72c4aa4
Vimal <vimalkumarak478@gmail.com>
2025-08-17 22:35:30 +0530

    new logout api create  vkc

--- controllers/auth/loginController.js
+++ controllers/auth/loginController.js
@@ -40,6 +40,26 @@ const loginController = {
             return next(err);
         }
 
+    },
+
+    //logout
+    async logout(req, res, next) {
+        // validate
+        const refreshSchema = Joi.object({
+            refresh_token: Joi.string().required(),
+        });
+        const { error } = refreshSchema.validate(req.body);
+                
+        if(error){
+            return next(error);
+        }
+        try {
+            await RefreshToken.deleteOne({ token: req.body.refresh_token });
+        } catch (err) {
+            return next(new Error('Something went wrong in the database'));
+        }
+
+        res.json({ status: 'ok'});
     }
 };
 
--- controllers/index.js
+++ controllers/index.js
@@ -2,4 +2,3 @@ export { default as registerController } from './auth/registerController.js';
 export { default as loginController } from './auth/loginController.js';
 export { default as userController } from './auth/userController.js';
 export { default as refreshController } from './auth/refreshController.js';
-
