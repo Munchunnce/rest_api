b3d86f592cd0dc69839b86e1947c71ce9c877ab0
Vimal <vimalkumarak478@gmail.com>
2025-08-19 19:54:04 +0530

    image path link url,  vkc

--- models/product.js
+++ models/product.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import { APP_URL } from "../config/index.js";
 
 const Schema = mongoose.Schema;
 
@@ -6,7 +7,10 @@ const productSchema = new Schema({
     name: { type: String, required: true },
     price: { type: Number, require: true },
     size: { type: String, require: true },
-    image: { type: String, require: true },
-}, { timestamps: true});
+    image: { type: String, require: true, get: (image) => {
+        // http://localhost:5000/uploads/1755529139439-209511301.15726292.png
+        return `${APP_URL}${image}`;
+    } },
+}, { timestamps: true, toJSON: { getters: true }});
 
 export default mongoose.model('Product', productSchema, 'products');
